1fa54d6d028c47e7fa7f2b1c0bb88c1da1ca3978
david zhang <896360979@qq.com>
2016-10-19 22:21:39 +0800

    Update get_date.js

--- get_date.js
+++ get_date.js
@@ -1,9 +1,9 @@
 /**
-   * 返回 相隔指定天数的时间戳
-   * @param  {int} timeLong 整数天
-   * @return {int}          时间戳
-   */
-  static getDate(timeLong){
-    let currentTime = parseInt(new Date().valueOf() / 1000) + (timeLong * 24 * 60 * 60);
-    return Util.timeToDateString(currentTime);
-  }
+ * 返回 相隔指定天数的时间戳
+ * @param  {int} timeLong 整数天
+ * @return {int}          时间戳
+ */
+static getDate(timeLong){
+  let currentTime = parseInt(new Date().valueOf() / 1000) + (timeLong * 24 * 60 * 60);
+  return Util.timeToDateString(currentTime);
+}
